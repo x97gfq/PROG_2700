4921511edf3646b3c989e090432b95b303494839
Jamie Symonds <jamie.symonds@gmail.com>
2023-01-19 21:58:57 -0400

    04 JSON

---
+++ 04_JSON/example1.js
@@ -0,0 +1,11 @@
+
+let str = "Welcome to NSCC";
+
+console.log("length of ", str, " is ",str.length);
+
+for (let i = 0; i < str.length; i++) {
+    console.log("letter at position ",i," is ",str[i]);
+}
+
+
+
---
+++ 04_JSON/example4.js
@@ -0,0 +1,30 @@
+
+var json = {
+    "students" : [
+        {
+            "name" : "Fred",
+            "grade" : 87
+        },
+        {
+            "name" : "Barney",
+            "grade" : 92
+        }
+    ]
+}
+
+console.log(json);
+
+console.log("using for-each:")
+for (var i = 0; i < json.students.length; i++) {
+    console.log(json.students[i].name);
+    console.log(json.students[i].grade);
+}
+
+console.log("using forEach:")
+json.students.forEach(function (item, index) {
+    console.log(item);
+    //console.log(item.name);
+    //console.log(item.grade);
+  });
+  
+
