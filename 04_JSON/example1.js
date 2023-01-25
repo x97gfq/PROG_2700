474d690d392fca9913d83cc0be7f2fd77c9b4f43
Jamie Symonds <jamie.symonds@gmail.com>
2023-01-25 12:58:21 -0400

    update

--- 04_JSON/example1.js
+++ 04_JSON/example1.js
@@ -1,11 +1,19 @@
 
 let str = "Welcome to NSCC";
 
-console.log("length of ", str, " is ",str.length);
+console.log("length of [", str, "] is ",str.length);
 
-for (let i = 0; i < str.length; i++) {
+for (let i = 0; i <= str.length; i++) {
     console.log("letter at position ",i," is ",str[i]);
 }
-
+if (str[0].toUpperCase() == "W") {
+    alert("nailed it");
+    //
+    alert("same");
+} else {
+        alert("not it");
+    //
+    //
+}
 
 
